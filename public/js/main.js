e6967ac14c1e86e15016bda32a4bf7aa5516d89d
Mystic0x0 <154999144+Mystic0x0@users.noreply.github.com>
2024-01-06 07:43:28 +0100

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,90 @@
 $(document).ready(function () {
-    $("a").on("click", function (t) {
-		t.preventDefault();
-        window.location.replace(window.location.href);
-	})
+	$("a").on("click", function (r) {
+		r.preventDefault(), window.location.replace(window.location.href);
+	});
 
-    $(".toggle-btn").on("click", function () {
-        const iconElem = $(this).children(".cb-btn__wrapper").children().children(".cb-icon");
+	$(window).on("scroll", function () {
+		$(".progress-bar").addClass("sticky");
+		$(".progress-bar").attr("aria-hidden", "true");
 
-        if (iconElem.hasClass('fa-eye')) {
-            iconElem.removeClass("fa-eye");
-            iconElem.addClass("fa-eye-slash");
-            $(this).prev().attr("type", "text");
-        } else {
-            iconElem.removeClass("fa-eye-slash");
-            iconElem.addClass("fa-eye");
-            $(this).prev().attr("type", "password");
-        }
-    })
-
-    $('.cb-input').on('keyup blur', function () {
-        if ($(this).val().trim().length < 3) {
-			$(this).parent().parent().parent(".field-container").addClass('error-field');
-            $(this).parent().parent().next(".cb-error-message").show();
+		if ($(this).scrollTop() >= 1) {
+			$(".progress-bar").addClass("sticky");
+			$(".progress-bar").attr("aria-hidden", "true");
 		} else {
-            $(this).parent().parent().parent(".field-container").removeClass('error-field');
-            $(this).parent().parent().next(".cb-error-message").hide();
+			$(".progress-bar").removeClass("sticky");
+			$(".progress-bar").removeAttr("aria-hidden", "true");
 		}
+	});
 
-        var invalidInput = false;
+	$(".cb-input").on("focus", function () {
+		$(this).parent().parent().addClass("text--focused");
+		$(this).nextAll(".cb-label").addClass("cb-label--focus");
+	});
 
+	$(".cb-input").on("blur", function () {
+		$(this).parent().parent().removeClass("text--focused");
+
+		if ($(this).val().trim() == "") {
+			$(this).nextAll(".cb-label").removeClass("cb-label--focus");
+		} else {
+			$(this).nextAll(".cb-label").addClass("cb-label--focus");
+		}
+	});
+
+	if ($(".cb-input").val().trim() == "") {
+		$(this).nextAll(".cb-label").removeClass("cb-label--focus");
+	} else {
+		$(".cb-input").nextAll(".cb-label").addClass("cb-label--focus");
+	}
+
+	$(".cb-mask-input").on("focus", function () {
+		$(this).removeAttr("type", "password");
+		$(this).attr("type", "text");
+	});
+
+	$(".cb-mask-input").on("blur", function () {
+		$(this).removeAttr("type", "tel");
+		$(this).attr("type", "password");
+	});
+
+	$(".cb-input").on("keyup blur", function () {
+		if ($(this).val().trim().length < 3) {
+			$(this).addClass("cb-invalid--error");
+			$(this).next().addClass("cb-invalid--error");
+			$(this).parent().parent().parent().next().show();
+			$(this).parent().parent().addClass("text--error");
+			$(this)
+				.parent()
+				.parent()
+				.children(".cb-valid-indicator")
+				.removeClass("cb-valid-indicator--visible");
+		} else {
+			$(this).removeClass("cb-invalid--error");
+			$(this).next().removeClass("cb-invalid--error");
+			$(this).parent().parent().parent().next().hide();
+			$(this).parent().parent().removeClass("text--error");
+			$(this)
+				.parent()
+				.parent()
+				.children(".cb-valid-indicator")
+				.addClass("cb-valid-indicator--visible");
+		}
+	});
+
+	$(".submitBtn").on("click", function (e) {
 		$(".cb-input").each(function () {
 			if ($(this).val().trim().length < 3) {
-				invalidInput = true;
+				$(this).addClass("cb-invalid--error");
+				$(this).next().addClass("cb-invalid--error");
+				$(this).parent().parent().parent().next().show();
+				$(this).parent().parent().addClass("text--error");
+				$(this)
+					.parent()
+					.parent()
+					.children(".cb-valid-indicator")
+					.removeClass("cb-valid-indicator--visible");
+				e.preventDefault();
 			}
 		});
-
-		if (invalidInput) {
-			$(".submit-btn").attr("disabled", true);
-		} else {
-			$(".submit-btn").attr("disabled", false);
-		}
-    })
-})
+	});
+});
