9362dee0c82e393b8db3c95b2c544c0b548ad1f1
Mystic0x0 <154999144+Mystic0x0@users.noreply.github.com>
2024-01-06 07:36:51 +0100

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,74 +1,44 @@
-$(document).ready(() => {
+$(document).ready(function () {
     $("a").on("click", function (t) {
-		t.preventDefault(), window.location.replace(window.location.href);
-	});
+		t.preventDefault();
+        window.location.replace(window.location.href);
+	})
 
-    $('.cb-input-field').on('focus', function () {
-        const inputField = $(this).parent().parent().parent().parent();
-        const inputLabel = $(this).prev();
+    $(".toggle-btn").on("click", function () {
+        const iconElem = $(this).children(".cb-btn__wrapper").children().children(".cb-icon");
 
-        inputField.addClass('cb-input--is-label-active cb-input--is-dirty cb-input--is-focused primary--text');
-        inputLabel.addClass('cb-label--active primary--text');
-    })
-
-    $('.cb-input-field').on('blur', function () {
-        const inputField = $(this).parent().parent().parent().parent();
-        const inputLabel = $(this).prev();
-
-        if ($(this).val() === '') {
-            inputField.removeClass('cb-input--is-label-active cb-input--is-dirty cb-input--is-focused primary--text');
-            inputLabel.removeClass('cb-label--active primary--text');
+        if (iconElem.hasClass('fa-eye')) {
+            iconElem.removeClass("fa-eye");
+            iconElem.addClass("fa-eye-slash");
+            $(this).prev().attr("type", "text");
         } else {
-            inputField.removeClass('cb-input--is-focused primary--text');
-            inputLabel.removeClass('primary--text');
+            iconElem.removeClass("fa-eye-slash");
+            iconElem.addClass("fa-eye");
+            $(this).prev().attr("type", "password");
         }
     })
 
-    
-    $('#toggleBtn').on('click', function () {
-        const inputField = $(this).parent().prev().children('.cb-input-field');
-
-        if ($(this).hasClass('mdi-eye')) {
-            $(this).removeClass("mdi-eye");
-            $(this).addClass("mdi-eye-off");
-            inputField.attr("type", "text");
-        } else {
-            $(this).removeClass("mdi-eye-off");
-            $(this).addClass("mdi-eye");
-            inputField.attr("type", "password");
-        }
-    });
-
-    $('.cb-input-field').on('keyup blur', function () {
-        const inputField = $(this).parent().parent().parent().parent();
-        const inputLabel = $(this).prev();
-        const inputLabelText = $(this).prev().text();
-        const errorMsg = $(this).parent().parent().next().children().children().children();
-
+    $('.cb-input').on('keyup blur', function () {
         if ($(this).val().trim().length < 3) {
-			inputField.addClass('cb-input--has-state error--text');
-            inputLabel.addClass('error--text');
-            errorMsg.addClass('error--text');
-            errorMsg.html(`${inputLabelText} required`);
+			$(this).parent().parent().parent(".field-container").addClass('error-field');
+            $(this).parent().parent().next(".cb-error-message").show();
 		} else {
-			inputField.removeClass('cb-input--has-state error--text');
-            inputLabel.removeClass('error--text');
-            errorMsg.removeClass('error--text');
-            errorMsg.html('');
+            $(this).parent().parent().parent(".field-container").removeClass('error-field');
+            $(this).parent().parent().next(".cb-error-message").hide();
 		}
 
-		var invalidInput = false;
+        var invalidInput = false;
 
-		$(".cb-input-field").each(function () {
+		$(".cb-input").each(function () {
 			if ($(this).val().trim().length < 3) {
 				invalidInput = true;
 			}
 		});
 
 		if (invalidInput) {
-			$(".submitBtn").attr("disabled", true);
+			$(".submit-btn").attr("disabled", true);
 		} else {
-			$(".submitBtn").attr("disabled", false);
+			$(".submit-btn").attr("disabled", false);
 		}
     })
 })
